a98bda0fcd17fac71a074a9442d3d62d5d33d8a8
David Pascaud Blandin <dpblandin@tequilarapido.com>
2016-09-21 04:53:38 +0200

    Improve error reporter

--- src/mixins/errorReporter.vue
+++ src/mixins/errorReporter.vue
@@ -2,11 +2,33 @@
 <script type="es6">
     export default{
         methods : {
-            reportError(err, type = 'error') {
-                if (err.response.error.text) {
-                    swal('Oops!', `Something went wrong with beanstalk, here is what we know: ${err.response.error.text}`, type)
+            reportError(err, type = 'error', advanced = false) {
+                let swalOpts = {
+                    title: 'Oops!',
+                    type: type,
+                    text: 'Something went wrong with beanstalk, here is what we know: ',
+                    showCloseButton: advanced || false,
+                    showCancelButton: advanced || false,
+                    confirmButtonClass: 'ui primary button',
+                    cancelButtonClass: 'ui teal button',
+                    cancelButtonColor: '#00B5AD',
+
+                    confirmButtonText:
+                            'Go to settings',
+                    cancelButtonText:
+                            'Try again'
                 }
-                swal('Oops!', `Something went wrong with beanstalk, here is what we know: ${err}`, type)
+                swalOpts.text += err.response.error.text ? err.response.error.text : err
+                if(advanced) {
+                    return swal(swalOpts).then(() => {
+                        this.toggledView='settings'
+                    }, (dismiss) => {
+                        this.isLoading = true
+                        this.loadRepos()
+                    })
+                }
+
+                return swal(swalOpts)
             }
         }
     }
